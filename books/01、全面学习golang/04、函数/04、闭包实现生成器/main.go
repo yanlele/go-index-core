2954d8e80f01cfc4a2fc9d220931b83fccdd9e53
yanlele <331393627@qq.com>
2020-10-11 23:19:02 +0800

    feat: 简单应用而已

---
+++ books/01、全面学习golang/04、函数/04、闭包实现生成器/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func playerGen(name string) func() (string, int) {
+	hp := 50
+	return func() (string, int) {
+		return name, hp
+	}
+}
+
+func main() {
+	generator := playerGen("yanle")
+	name, ph := generator()
+	fmt.Println(name, ph)
+}
