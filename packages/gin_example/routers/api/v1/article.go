75cd132c94d9caaae9586838818f155ace5415aa
yanlele <331393627@qq.com>
2020-11-29 23:15:08 +0800

    feat: 添加article 相关的路由

---
+++ packages/gin_example/routers/api/v1/article.go
@@ -0,0 +1,23 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func GetArticle(context *gin.Context) {
+
+}
+
+func GetArticles(context *gin.Context) {
+
+}
+
+func AddArticle(context *gin.Context) {
+
+}
+
+func EditArticle(context *gin.Context) {
+
+}
+
+func DeleteArticle(context *gin.Context) {
+
+}
