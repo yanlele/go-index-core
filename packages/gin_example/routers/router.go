72a98b1c4715312409745137ae82b6de4c5be1c0
yanlele <331393627@qq.com>
2020-11-29 12:04:41 +0800

    feat: 添加路由

--- packages/gin_example/routers/router.go
+++ packages/gin_example/routers/router.go
@@ -2,18 +2,25 @@ package routers
 
 import (
 	"gin-example/pkg/setting"
+	v1 "gin-example/routers/api/v1"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
 	gin.SetMode(setting.RunMode)
-	router.GET("/test", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "test",
-		})
-	})
+
+	apiv1 := router.Group("/api/v1")
+	{
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		//更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
 
 	return router
 }
