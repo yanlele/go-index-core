4d15f430de8c91eb1804cd21a268bbf253e872a0
yanlele <331393627@qq.com>
2020-11-29 23:34:15 +0800

    feat: 添加 article model 逻辑

---
+++ packages/gin_example/models/article.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Article struct {
+	Model
+
+	TagID         int    `json:"tag_id" gorm:"index"`
+	Tag           Tag    `json:"tag"`
+	Title         string `json:"title"`
+	Desc          string `json:"desc"`
+	Content       string `json:"content"`
+	CoverImageUrl string `json:"cover_image_url"`
+	CreatedBy     string `json:"created_by"`
+	ModifiedBy    string `json:"modified_by"`
+	State         int    `json:"state"`
+}
+
+/* 通过 id 判定是否存在 */
+func ExistArticleByID(id int) bool {
+	var article Article
+	db.Select("id").Where("id = ?", id).First(&article)
+	if article.ID > 0 {
+		return true
+	}
+	return false
+}
+
+/* 获取文章总数 */
+func GetArticleTotal(maps interface{}) (int64, error) {
+	var count int64
+	if err := db.Model(&Article{}).Where(maps).Count(&count).Error; err != nil {
+		return count, err
+	}
+	return count, nil
+}
+
+/* 获取所有文章 */
+func GetArticles(pageNum int, pageSize int, maps interface{}) ([]*Article, error) {
+	var articles []*Article
+	err := db.Preload("Tag").Where(maps).Offset(pageNum).Limit(pageSize).Find(&articles).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return articles, err
+}
+
+/*  */
+
+func (article *Article) BeforeCreate() error {
+	article.CreatedOn = time.Now().Unix()
+	return nil
+}
+
+func (article *Article) BeforeUpdate() error {
+	article.ModifiedOn = time.Now().Unix()
+	return nil
+}
