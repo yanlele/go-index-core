d91ba24037fcbfcd034204929d82b18143d228f6
yanlele <331393627@qq.com>
2020-12-05 13:16:31 +0800

    feat: 添加log 文件

---
+++ packages/gin_example/pkg/logging/log.go
@@ -0,0 +1,23 @@
+package logging
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	LogSavePath = "runtime/logs/"
+	LogSaveName = "log"
+	LogFileExt  = "log"
+	TimeFormat  = "20200102"
+)
+
+func getLogFilePath() string {
+	return fmt.Sprintf("%s", LogSavePath)
+}
+
+func getLogFileFullPath() string {
+	prefixPath := getLogFilePath()
+	suffixPath := fmt.Sprintf("%s%s.%s", LogSaveName, time.Now().Format(TimeFormat), LogFileExt)
+	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
+}
