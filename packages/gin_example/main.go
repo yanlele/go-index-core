ebfb254a04781cd01bb12ce693ff0bd540c15f8d
yanlele <331393627@qq.com>
2020-12-05 15:17:19 +0800

    feat: 添加endless

--- packages/gin_example/main.go
+++ packages/gin_example/main.go
@@ -4,12 +4,32 @@ import (
 	"fmt"
 	"gin-example/pkg/setting"
 	"gin-example/routers"
+	"github.com/fvbock/endless"
+	"log"
+	"syscall"
 )
 
 func main() {
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	_ = router.Run(fmt.Sprintf(":%d", setting.HTTPPort))
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
+
+
+	//router := routers.InitRouter()
+	//_ = router.Run(fmt.Sprintf(":%d", setting.HTTPPort))
+
+
 	//s := &http.Server{
 	//	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
 	//	Handler:        router,
