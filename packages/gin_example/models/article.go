dc5cee2afa63437c2c916d6aa6a7cfb68ce12a52
yanlele <331393627@qq.com>
2020-12-07 09:01:18 +0800

    feat: 一个简单的定时任务

--- packages/gin_example/models/article.go
+++ packages/gin_example/models/article.go
@@ -9,15 +9,15 @@ import (
 type Article struct {
 	Model
 
-	TagID         int    `json:"tag_id" gorm:"index"`
-	Tag           Tag    `json:"tag"`
-	Title         string `json:"title"`
-	Desc          string `json:"desc"`
-	Content       string `json:"content"`
+	TagID   int    `json:"tag_id" gorm:"index"`
+	Tag     Tag    `json:"tag"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 	//CoverImageUrl string `json:"cover_image_url"`
-	CreatedBy     string `json:"created_by"`
-	ModifiedBy    string `json:"modified_by"`
-	State         int    `json:"state"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State      int    `json:"state"`
 }
 
 /* 通过 id 判定是否存在 */
@@ -83,6 +83,11 @@ func AddArticle(data map[string]interface{}) bool {
 
 func DeleteArticle(id int) bool {
 	db.Where("id = ?", id).Delete(&Article{})
+	return true
+}
+
+func CleanAllArticle() bool {
+	db.Unscoped().Where("deleted_on != ?", 0).Delete(&Article{})
 	return true
 }
 
