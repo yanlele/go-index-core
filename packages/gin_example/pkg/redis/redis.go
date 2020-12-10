1cc627cf349c4d604f34683a0cb9a2826d328d04
yanlele <331393627@qq.com>
2020-12-11 00:17:15 +0800

    feat: 封装 redis 的一些基本操作功能

--- packages/gin_example/pkg/redis/redis.go
+++ packages/gin_example/pkg/redis/redis.go
@@ -1,10 +1,110 @@
 package redis
 
-import "github.com/gomodule/redigo/redis"
+import (
+	"encoding/json"
+	"gin-example/pkg/setting"
+	"github.com/gomodule/redigo/redis"
+	"time"
+)
 
 var RedisConn *redis.Pool
 
 func SetUp() error {
 	RedisConn = &redis.Pool{
+		MaxIdle:     setting.RedisSetting.MaxIdle,
+		MaxActive:   setting.RedisSetting.MaxActive,
+		IdleTimeout: setting.RedisSetting.IdleTimeout,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", setting.RedisSetting.Host)
+			if err != nil {
+				return nil, err
+			}
+
+			// 有密码的场景
+			if setting.RedisSetting.Password != "" {
+				if _, err := c.Do("AUTH", setting.RedisSetting.Password); err != nil {
+					_ = c.Close()
+					return nil, err
+				}
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
 	}
+	return nil
+}
+
+func Set(key string, data interface{}, time int) error {
+	conn := RedisConn.Get()
+	defer conn.Close()
+
+	// 转为json string
+	value, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// 设置值
+	_, err = conn.Do("SET", key, value)
+	if err != nil {
+		return err
+	}
+
+	// 设置超时时间
+	_, err = conn.Do("EXPIRE", key, time)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Exists(key string) bool {
+	conn := RedisConn.Get()
+	defer conn.Close()
+
+	exists, err := redis.Bool(conn.Do("EXISTS", key))
+	if err != nil {
+		return false
+	}
+	return exists
+}
+
+func Get(key string) ([]byte, error) {
+	conn := RedisConn.Get()
+	defer conn.Close()
+
+	replay, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return nil, err
+	}
+	return replay, nil
+}
+
+func Delete(key string) (bool, error) {
+	conn := RedisConn.Get()
+	defer conn.Close()
+
+	return redis.Bool(conn.Do("DEL", key))
+}
+
+func LikeDeletes(key string) error {
+	conn := RedisConn.Get()
+	defer conn.Close()
+
+	keys, err := redis.Strings(conn.Do("KEYS", "*"+key+"*"))
+	if err != nil {
+		return err
+	}
+
+	for _, key := range keys {
+		_, err := Delete(key)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
