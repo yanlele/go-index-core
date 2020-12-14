2ae73290cfb73c327a50ee058569893722352dba
yanlele <331393627@qq.com>
2020-12-15 00:02:28 +0800

    feat: 添加stream

---
+++ packages/grpc_example/server/stream_server/server.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	pb "gin-example/proto"
+	"io"
+	"log"
+)
+
+type StreamService struct {
+}
+
+const PORT = "9002"
+
+func main() {
+
+}
+
+func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListServer) error {
+	for n := 0; n < 6; n++ {
+		err := stream.Send(&pb.StreamResponse{
+			Pt: &pb.StreamPoint{
+				Name:  r.Pt.Name,
+				Value: r.Pt.Value + int32(n),
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
+	for {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.StreamResponse{
+				Pt: &pb.StreamPoint{
+					Name:  "gRPC Stream Server: Record",
+					Value: 1,
+				},
+			})
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+	}
+	return nil
+}
+
+func (s *StreamService) Route(stream pb.StreamService_RouteServer) error {
+	n := 0
+	for {
+		err:= stream.Send(&pb.StreamResponse{
+			Pt: &pb.StreamPoint{
+				Name:  "gPRC Stream Client: Route",
+				Value: int32(n),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		r, err:= stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		n++
+
+		log.Printf("stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+	}
+	return nil
+}
