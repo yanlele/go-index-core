3cdf9b599297ace2d79fb9b61b1a217c62b5fe31
yanlele <331393627@qq.com>
2020-12-17 23:42:25 +0800

    feat: 添加session

--- packages/le_blog/bootstrap/init.go
+++ packages/le_blog/bootstrap/init.go
@@ -1,6 +1,8 @@
 package bootstrap
 
 import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,5 +10,14 @@ const CookieSessionKey = "blog_session"
 
 func Init() *gin.Engine {
 	app := gin.Default()
+
+	// 完全不知道这个有啥用？
+	//gob.Register(controllers.Auth{})
+
+	// 添加cookie 和 session
+	store := cookie.NewStore([]byte("secret"))
+	app.Use(sessions.Sessions(CookieSessionKey, store))
+
+
 	return app
 }
