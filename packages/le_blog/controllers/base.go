e83e6f805e86dff4968d9e64e3da721a00b4a389
yanlele <331393627@qq.com>
2020-12-17 23:35:09 +0800

    feat: 添加auth 方法

---
+++ packages/le_blog/controllers/base.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+type Auth struct {
+	Id         int
+	Name       string
+	Avatar     string
+	Profession string
+}
+
+func (a Auth) GetAuth(c *gin.Context) Auth {
+	auth, exists := c.Get("auth")
+	if !exists {
+		auth = Auth{
+			Id:         0,
+			Name:       "",
+			Avatar:     "/static/logoh.png",
+			Profession: "",
+		}
+	}
+	return auth.(Auth)
+}
