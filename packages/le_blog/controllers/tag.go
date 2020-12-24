6e56df4561b725a5fc7631dc342a3474dbcc521b
yanlele <331393627@qq.com>
2020-12-24 23:43:51 +0800

    feat: GetArticleByTagName

--- packages/le_blog/controllers/tag.go
+++ packages/le_blog/controllers/tag.go
@@ -26,3 +26,31 @@ func TagIndex(c *gin.Context) {
 
 	c.HTML(http.StatusOK, "tagIndex", data)
 }
+
+// 标签
+func GetArticleByTagName(c *gin.Context) {
+	tagName := c.Param("id")
+	if tagName == "" {
+		utils.RedirectBack(c)
+		return
+	}
+
+	var articles []modules.Article
+	//dbQuery := driver.Db.Table("articles").Where("find_in_set('" + tagName + "', tags)").Order("view_num")
+	dbQuery := driver.DB.Table("articles").Where("find_in_set('" + tagName + "', tags)").Order("view_num")
+
+	paginate, err := (&modules.Pagination{}).Paginate(c, *dbQuery, &articles)
+
+	if err != nil {
+		panic(err)
+	}
+
+	auth := Auth{}.GetAuth(c)
+	header := Header{"标签"}
+	data := struct {
+		Paginate modules.Pagination
+		Auth
+		Header
+	}{*paginate, auth, header}
+	c.HTML(http.StatusOK, "index", data)
+}
