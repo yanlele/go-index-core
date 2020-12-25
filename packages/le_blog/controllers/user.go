2346351d8586f1d4375f9204d818d978fb36251d
yanlele <331393627@qq.com>
2020-12-25 21:59:48 +0800

    feat: DoRegister

--- packages/le_blog/controllers/user.go
+++ packages/le_blog/controllers/user.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"le-blog/bootstrap/driver"
+	"le-blog/modules"
 	"le-blog/utils"
 	"net/http"
 )
@@ -26,4 +30,83 @@ func Register(c *gin.Context) {
 	}{"注册", auth}
 
 	c.HTML(http.StatusOK, "join", data)
+}
+
+func DoRegister(c *gin.Context) {
+	var regData formUser
+	if err := c.ShouldBind(&regData); err != nil {
+		response := utils.Response{
+			Status: 403,
+			Data:   nil,
+			Msg:    err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response.FailResponse())
+		return
+	}
+	var user modules.User
+
+	// 保存数据到数据库
+	driver.DB.Where("email = ?", regData.Email).First(&user)
+
+	if user.ID != 0 {
+		res := utils.Response{
+			Status: 1001,
+			Data:   nil,
+			Msg:    "邮箱已经存在",
+		}
+		c.JSON(http.StatusOK, res.FailResponse())
+		return
+	}
+
+	salt := utils.Salt()
+	user = modules.User{
+		Name:       regData.Name,
+		Password:   utils.CryptUserPassword(regData.Password, salt),
+		Salt:       salt,
+		Email:      regData.Email,
+		Profession: "",
+		Avatar:     "",
+	}
+
+	// 保存数据
+	err := driver.DB.Create(&user).Error
+	if err != nil {
+		res := utils.Response{
+			Status: 500,
+			Data:   nil,
+			Msg:    err.Error(),
+		}
+		c.JSON(http.StatusOK, res.FailResponse())
+		return
+	}
+
+	// 将用户数据写入session
+	auth := &Auth{
+		Id:         int(user.ID),
+		Name:       user.Name,
+		Avatar:     user.Avatar,
+		Profession: user.Profession,
+	}
+
+	session := sessions.Default(c)
+	session.Set("auth", auth)
+	err = session.Save()
+
+	fmt.Println(session.Get("auth"))
+
+	if err != nil {
+		res := utils.Response{
+			Status: 500,
+			Data:   nil,
+			Msg:    err.Error(),
+		}
+		c.JSON(http.StatusOK, res.FailResponse())
+		return
+	}
+
+	// 成功的场景
+	res := utils.Response{
+		Data: regData,
+	}
+	c.JSON(http.StatusOK, res.SuccessResponse())
 }
