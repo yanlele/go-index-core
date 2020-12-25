76c5a303a46de1cccfff8964a5095f4179a97a2c
yanlele <331393627@qq.com>
2020-12-25 22:20:53 +0800

    feat: 添加一个关于指针的demo

---
+++ demos/20年/12月/02_指针/01_demo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type User struct {
+	name string
+	age  int
+}
+
+func (user *User) get() {
+	fmt.Println("name: ", user.name)
+	fmt.Println("age: ", user.age)
+}
+
+func (user User) getUser() {
+	fmt.Println("user name :", user.name)
+}
+
+func main() {
+	user := User{
+		"YANLE",
+		29,
+	}
+
+	(&User{
+		"Yanlele",
+		29,
+	}).get()
+
+	user.get()
+
+	User{
+		"yanle",
+		29,
+	}.getUser()
+}
