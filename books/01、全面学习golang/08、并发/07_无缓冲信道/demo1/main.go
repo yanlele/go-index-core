f88446368677d7d9b61deb65777c5849d67d6976
yanlele <331393627@qq.com>
2020-12-31 22:28:13 +0800

    feat: 无缓冲信道demo1

--- books/01、全面学习golang/08、并发/07_无缓冲信道/demo1/main.go
+++ books/01、全面学习golang/08、并发/07_无缓冲信道/demo1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"sync"
 	"time"
@@ -14,8 +15,31 @@ func init() {
 
 func player(name string, court chan int) {
 	defer wg.Done()
+	for {
+		ball, ok := <-court
+		if !ok {
+			fmt.Printf("Player %s Won\n", name)
+			return
+		}
+
+		n := rand.Intn(100)
+		if n%13 == 0 {
+			fmt.Printf("Player %s Missed\n", name)
+			close(court)
+			return
+		}
+
+		fmt.Printf("Player %s Hit %d\n", name, ball)
+		ball++
+		court <- ball
+	}
 }
 
 func main() {
-
+	court := make(chan int)
+	wg.Add(2)
+	go player("yanle", court)
+	go player("lele", court)
+	court <- 1
+	wg.Wait()
 }
