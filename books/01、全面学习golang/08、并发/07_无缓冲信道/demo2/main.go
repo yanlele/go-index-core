9fa1d24e0ae42972c41a3ac9265fe4798ca99355
yanlele <331393627@qq.com>
2021-01-01 22:13:36 +0800

    feat: handleRunner

--- books/01、全面学习golang/08、并发/07_无缓冲信道/demo2/main.go
+++ books/01、全面学习golang/08、并发/07_无缓冲信道/demo2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
@@ -16,7 +17,15 @@ func handleRunner(baton chan int) {
 		fmt.Printf("Runner %d To The Line\n", newRunner)
 		go handleRunner(baton)
 	}
+	time.Sleep(100 * time.Millisecond)
+	if runner == 4 {
+		fmt.Printf("Runner %d Finished, Race Over\n", runner)
+		wg.Done()
+		return
+	}
 
+	fmt.Printf("Runner %d Exchange Whit Runner %d \n", runner, newRunner)
+	baton <- newRunner
 }
 
 func main() {
