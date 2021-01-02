917b9d134eefb8f08271a901f0f416b84369c469
yanlele <331393627@qq.com>
2021-01-02 23:19:47 +0800

    feat: 模拟客户端接收信息

--- books/01、全面学习golang/08、并发/11_模拟远程控制/main.go
+++ books/01、全面学习golang/08、并发/11_模拟远程控制/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"errors"
+	"time"
+)
+
+// 模拟客户端接收信息
+func RPCClient(ch chan string, req string) (string, error) {
+	ch <- req
+	select {
+	case ack := <-ch:
+		return ack, nil
+	case <-time.After(time.Second):
+		return "", errors.New("time out")
+	}
+}
 
 func main() {
-	
 }
